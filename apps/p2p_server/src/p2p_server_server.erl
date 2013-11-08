e2f32dbec07e811806edaf8a22f307002f1f7b05
Johnson Liu <jliu@xtremeprog.com>
2013-11-08 18:20:11 +0800

    got

--- apps/p2p_server/src/p2p_server_server.erl
+++ apps/p2p_server/src/p2p_server_server.erl
@@ -2,12 +2,14 @@
 -include("p2p_server.hrl").
 -behaviour(gen_server).
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 -record(state, {
-                socket
+                lsocket,
+                socket,
+                parent
                }).
 
 
@@ -15,8 +17,8 @@
 %% API functions
 %% ===================================================================
 
-start_link() ->
-    gen_server:start_link(?MODULE, [], []).
+start_link(LSocket) ->
+    gen_server:start_link(?MODULE, [], [LSocket, erlang:self()]).
 
 
 %% ===================================================================
@@ -24,16 +26,14 @@ start_link() ->
 %% ===================================================================
 
 %% -- init -----------
-init([]) ->
-    {ok, UdpPort} = application:get_env(udp_port),
-    {ok, Socket} = gen_udp:open(UdpPort, [binary]),
-
+init([LSocket, Parent]) ->
     State = #state{
-        socket = Socket
+        lsocket = LSocket,
+        parent = Parent
     },
 
-    %error_logger:info_msg("[~p] was started with state ~p.~n", [?MODULE, State]),
-    {ok, State}.
+    error_logger:info_msg("[~p] was started.~n", [?MODULE]),
+    {ok, State, 0}.
 
 
 %% -- call -----------
@@ -49,14 +49,36 @@ handle_cast(_Msg, State) ->
 
 
 %% -- info -------------
-handle_info({udp, _UdpSocket, Ip, Port, RawData}, State) ->
-    %error_logger:info_msg("[~p] received udp data(~p:~p): ~p~n", [?MODULE, Ip, Port, RawData]),
-    handle_data(Ip, Port, RawData, State#state.socket),
+handle_info({tcp, _Socket, RawData}, State) ->
+    error_logger:info_msg("[~p] received tcp data: ~p~n", [?MODULE, RawData]),
     {noreply, State};
+
+handle_info({tcp_closed, _Socket}, State) ->
+    error_logger:info_msg("[~p] was infoed: ~p.~n", [?MODULE, tcp_closed]),
+    {stop, tcp_closed, State};
+
+handle_info(timeout, State) ->    
+    #state{
+        lsocket = LSocket,
+        socket = Socket0,
+        parent = Parent
+    } = State,
+
+    State2 = case Socket0 of
+        undefined ->
+            {ok, Socket} = gen_tcp:accept(LSocket),
+            p2p_server_sup:start_child(Parent),
+            State#state{socket = Socket};
+        _ ->
+            State
+    end,
+
+    {noreply, State2};
 
 handle_info(_Msg, State) ->
     error_logger:info_msg("[~p] was infoed: ~p.~n", [?MODULE, _Msg]),
     {noreply, State}.
+
 
 %% -- terminate ---------
 terminate(Reason, _State) ->
@@ -72,108 +94,108 @@ code_change(_OldVsn, State, _Extra) ->
 %% Local Functions
 %% ===================================================================
 
-handle_data(Ip, Port, RawData, Socket) ->
-    <<TypeCode:1/binary, DataLen:8/integer, _/binary>> = RawData,
-    Payload = binary:part(RawData, 2, DataLen), 
-
-    case TypeCode of
-        %% -- online ---------------
-        <<16#01>> -> 
-            handle_data_online_req(Ip, Port, Payload, Socket);
-
-        %% -- connect req ----------
-        <<16#02>> -> 
-            handle_data_connect_req(Ip, Port, Payload, Socket)
-    end.
-
-
-handle_data_online_req(Ip, Port, Payload, Socket) ->
-    error_logger:info_msg("[~p] client online(~p:~p): ~p~n", [?MODULE, Ip, Port, Payload]),
-    <<LocalIp1:8/integer, LocalIp2:8/integer, LocalIp3:8/integer, LocalIp4:8/integer, LocalPortH:8/integer, LocalPortL:8/integer, ClientIdData/binary>> = Payload,
-
-    LocalIp = {LocalIp1, LocalIp2, LocalIp3, LocalIp4}, 
-    LocalPort = LocalPortH * 256 + LocalPortL,
-
-    <<_ClientIdLen:8/integer, ClientId0/binary>> = ClientIdData,
-    ClientId = erlang:binary_to_list(ClientId0),
-
-    SendingData = case model_run_user:get(ClientId) of
-        undefined ->
-            model_run_user:create(#run_user{
-                    id = ClientId, 
-                    local_ip = LocalIp,
-                    local_port = LocalPort,
-                    public_ip = Ip,
-                    public_port = Port,
-					createdTime = tools:datetime_string('yyyyMMdd_hhmmss')
-            }),
-            %% success
-            <<16#01, 16#01, 16#00>>;
-        _ ->
-            %% Fail
-            <<16#01, 16#01, 16#01>>
-    end,
-
-    gen_udp:send(Socket, Ip, Port, SendingData).
-
-
-handle_data_connect_req(Ip, Port, Payload, Socket) ->
-    error_logger:info_msg("[~p] connect to peer req(~p:~p): ~p~n", [?MODULE, Ip, Port, Payload]),
-    <<ClientIdLen:8/integer, RestPayload/binary>> = Payload,
-    ClientId = erlang:binary_to_list(binary:part(RestPayload, 0, ClientIdLen)),
-    RestPayload2 = binary:part(RestPayload, ClientIdLen, erlang:byte_size(RestPayload) - ClientIdLen),
-    <<_PeerIdLen:8/integer, PeerId0/binary>> = RestPayload2,
-    PeerId = erlang:binary_to_list(PeerId0),
-
-    case model_run_user:get(ClientId) of
-        undefined ->
-            %% ClientId is not online
-            SendingData = <<16#02, 16#01, 16#01>>,
-            gen_udp:send(Socket, Ip, Port, SendingData);
-        error ->
-            %% db error
-            SendingData = <<16#02, 16#01, 16#01>>,
-            gen_udp:send(Socket, Ip, Port, SendingData);
-        Client ->
-            case model_run_user:get(PeerId) of
-                undefined ->
-                    %% PeerId is not online
-                    SendingData = <<16#02, 16#01, 16#01>>,
-                    gen_udp:send(Socket, Ip, Port, SendingData);
-                error ->
-                    %% db error
-                    SendingData = <<16#02, 16#01, 16#01>>,
-                    gen_udp:send(Socket, Ip, Port, SendingData);
-                Peer ->
-                    ClientIdLen = erlang:size(erlang:list_to_binary([ClientId])),
-                    PeerIdLen = erlang:size(erlang:list_to_binary([PeerId])),
-
-                    {ClientLocalIp1, ClientLocalIp2, ClientLocalIp3, ClientLocalIp4} = Client#run_user.local_ip, 
-                    {ClientPublicIp1, ClientPublicIp2, ClientPublicIp3, ClientPublicIp4} = Client#run_user.public_ip, 
-
-                    {PeerLocalIp1, PeerLocalIp2, PeerLocalIp3, PeerLocalIp4} = Peer#run_user.local_ip, 
-                    {PeerPublicIp1, PeerPublicIp2, PeerPublicIp3, PeerPublicIp4} = Peer#run_user.public_ip, 
-
-                    ClientLocalPortH = Client#run_user.local_port div 256,
-                    ClientLocalPortL = Client#run_user.local_port rem 256,
-
-                    ClientPublicPortH = Client#run_user.public_port div 256,
-                    ClientPublicPortL = Client#run_user.public_port rem 256,
-
-                    PeerLocalPortH = Peer#run_user.local_port div 256,
-                    PeerLocalPortL = Peer#run_user.local_port rem 256,
-
-                    PeerPublicPortH = Peer#run_user.public_port div 256,
-                    PeerPublicPortL = Peer#run_user.public_port rem 256,
-
-                    SendingDataLenPeer = 14 + ClientIdLen,
-                    SendingDataLenClient = 14 + PeerIdLen,
-
-                    SendingDataPeer = [16#02, SendingDataLenPeer, 16#00, ClientLocalIp1, ClientLocalIp2, ClientLocalIp3, ClientLocalIp4, ClientLocalPortH, ClientLocalPortL, ClientPublicIp1, ClientPublicIp2, ClientPublicIp3, ClientPublicIp4, ClientPublicPortH, ClientPublicPortL, ClientIdLen, ClientId],
-
-                    SendingDataClient = [16#02, SendingDataLenClient, 16#00, PeerLocalIp1, PeerLocalIp2, PeerLocalIp3, PeerLocalIp4, PeerLocalPortH, PeerLocalPortL, PeerPublicIp1, PeerPublicIp2, PeerPublicIp3, PeerPublicIp4, PeerPublicPortH, PeerPublicPortL, PeerIdLen, PeerId],
-
-                    gen_udp:send(Socket, Ip, Port, SendingDataClient),
-                    gen_udp:send(Socket, Peer#run_user.public_ip, Peer#run_user.public_port, SendingDataPeer)
-            end
-    end.
+%handle_data(Ip, Port, RawData, Socket) ->
+%    <<TypeCode:1/binary, DataLen:8/integer, _/binary>> = RawData,
+%    Payload = binary:part(RawData, 2, DataLen), 
+%
+%    case TypeCode of
+%        %% -- online ---------------
+%        <<16#01>> -> 
+%            handle_data_online_req(Ip, Port, Payload, Socket);
+%
+%        %% -- connect req ----------
+%        <<16#02>> -> 
+%            handle_data_connect_req(Ip, Port, Payload, Socket)
+%    end.
+%
+%
+%handle_data_online_req(Ip, Port, Payload, Socket) ->
+%    error_logger:info_msg("[~p] client online(~p:~p): ~p~n", [?MODULE, Ip, Port, Payload]),
+%    <<LocalIp1:8/integer, LocalIp2:8/integer, LocalIp3:8/integer, LocalIp4:8/integer, LocalPortH:8/integer, LocalPortL:8/integer, ClientIdData/binary>> = Payload,
+%
+%    LocalIp = {LocalIp1, LocalIp2, LocalIp3, LocalIp4}, 
+%    LocalPort = LocalPortH * 256 + LocalPortL,
+%
+%    <<_ClientIdLen:8/integer, ClientId0/binary>> = ClientIdData,
+%    ClientId = erlang:binary_to_list(ClientId0),
+%
+%    SendingData = case model_run_user:get(ClientId) of
+%        undefined ->
+%            model_run_user:create(#run_user{
+%                    id = ClientId, 
+%                    local_ip = LocalIp,
+%                    local_port = LocalPort,
+%                    public_ip = Ip,
+%                    public_port = Port,
+%					createdTime = tools:datetime_string('yyyyMMdd_hhmmss')
+%            }),
+%            %% success
+%            <<16#01, 16#01, 16#00>>;
+%        _ ->
+%            %% Fail
+%            <<16#01, 16#01, 16#01>>
+%    end,
+%
+%    gen_udp:send(Socket, Ip, Port, SendingData).
+%
+%
+%handle_data_connect_req(Ip, Port, Payload, Socket) ->
+%    error_logger:info_msg("[~p] connect to peer req(~p:~p): ~p~n", [?MODULE, Ip, Port, Payload]),
+%    <<ClientIdLen:8/integer, RestPayload/binary>> = Payload,
+%    ClientId = erlang:binary_to_list(binary:part(RestPayload, 0, ClientIdLen)),
+%    RestPayload2 = binary:part(RestPayload, ClientIdLen, erlang:byte_size(RestPayload) - ClientIdLen),
+%    <<_PeerIdLen:8/integer, PeerId0/binary>> = RestPayload2,
+%    PeerId = erlang:binary_to_list(PeerId0),
+%
+%    case model_run_user:get(ClientId) of
+%        undefined ->
+%            %% ClientId is not online
+%            SendingData = <<16#02, 16#01, 16#01>>,
+%            gen_udp:send(Socket, Ip, Port, SendingData);
+%        error ->
+%            %% db error
+%            SendingData = <<16#02, 16#01, 16#01>>,
+%            gen_udp:send(Socket, Ip, Port, SendingData);
+%        Client ->
+%            case model_run_user:get(PeerId) of
+%                undefined ->
+%                    %% PeerId is not online
+%                    SendingData = <<16#02, 16#01, 16#01>>,
+%                    gen_udp:send(Socket, Ip, Port, SendingData);
+%                error ->
+%                    %% db error
+%                    SendingData = <<16#02, 16#01, 16#01>>,
+%                    gen_udp:send(Socket, Ip, Port, SendingData);
+%                Peer ->
+%                    ClientIdLen = erlang:size(erlang:list_to_binary([ClientId])),
+%                    PeerIdLen = erlang:size(erlang:list_to_binary([PeerId])),
+%
+%                    {ClientLocalIp1, ClientLocalIp2, ClientLocalIp3, ClientLocalIp4} = Client#run_user.local_ip, 
+%                    {ClientPublicIp1, ClientPublicIp2, ClientPublicIp3, ClientPublicIp4} = Client#run_user.public_ip, 
+%
+%                    {PeerLocalIp1, PeerLocalIp2, PeerLocalIp3, PeerLocalIp4} = Peer#run_user.local_ip, 
+%                    {PeerPublicIp1, PeerPublicIp2, PeerPublicIp3, PeerPublicIp4} = Peer#run_user.public_ip, 
+%
+%                    ClientLocalPortH = Client#run_user.local_port div 256,
+%                    ClientLocalPortL = Client#run_user.local_port rem 256,
+%
+%                    ClientPublicPortH = Client#run_user.public_port div 256,
+%                    ClientPublicPortL = Client#run_user.public_port rem 256,
+%
+%                    PeerLocalPortH = Peer#run_user.local_port div 256,
+%                    PeerLocalPortL = Peer#run_user.local_port rem 256,
+%
+%                    PeerPublicPortH = Peer#run_user.public_port div 256,
+%                    PeerPublicPortL = Peer#run_user.public_port rem 256,
+%
+%                    SendingDataLenPeer = 14 + ClientIdLen,
+%                    SendingDataLenClient = 14 + PeerIdLen,
+%
+%                    SendingDataPeer = [16#02, SendingDataLenPeer, 16#00, ClientLocalIp1, ClientLocalIp2, ClientLocalIp3, ClientLocalIp4, ClientLocalPortH, ClientLocalPortL, ClientPublicIp1, ClientPublicIp2, ClientPublicIp3, ClientPublicIp4, ClientPublicPortH, ClientPublicPortL, ClientIdLen, ClientId],
+%
+%                    SendingDataClient = [16#02, SendingDataLenClient, 16#00, PeerLocalIp1, PeerLocalIp2, PeerLocalIp3, PeerLocalIp4, PeerLocalPortH, PeerLocalPortL, PeerPublicIp1, PeerPublicIp2, PeerPublicIp3, PeerPublicIp4, PeerPublicPortH, PeerPublicPortL, PeerIdLen, PeerId],
+%
+%                    gen_udp:send(Socket, Ip, Port, SendingDataClient),
+%                    gen_udp:send(Socket, Peer#run_user.public_ip, Peer#run_user.public_port, SendingDataPeer)
+%            end
+%    end.
--- apps/p2p_server/src/p2p_server_sup.erl
+++ apps/p2p_server/src/p2p_server_sup.erl
@@ -1,9 +1,11 @@
 -module(p2p_server_sup).
 -behaviour(supervisor).
 %% API
--export([start_link/0]).
+-export([start_link/0, start_child/1]).
 %% Supervisor callbacks
 -export([init/1]).
+
+-define(SERVER, ?MODULE).
 
 
 %% ===================================================================
@@ -11,14 +13,37 @@
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    {ok, SupervisorPid} = supervisor:start_link({local, ?SERVER}, ?MODULE, []),
+
+    %% start child servers for connection listening
+    {ok, WorkerCount} = application:get_env(p2p_server, init_worker_count),
+    start_childs(SupervisorPid, WorkerCount),
+
+    {ok, SupervisorPid}.
+
+
+start_childs(_, 0) ->
+    ok;
+start_childs(SupervisorPid, WorkerCount) ->
+    supervisor:start_child(SupervisorPid, []),
+    start_childs(SupervisorPid, WorkerCount - 1).
+
+
+start_child(SupervisorPid) ->
+    supervisor:start_child(SupervisorPid, []).
 
 
 init([]) ->
-    Server = {p2p_server_server, {p2p_server_server, start_link, []},
-              permanent, 10000, worker, [p2p_server_server]},
+    {ok, Port} = application:get_env(p2p_server, tcp_port),
+	SocketOpts = [binary, {active, true}, {reuseaddr, true}],
+    {ok, LSocket} = gen_tcp:listen(Port, SocketOpts),
 
-    {ok, {{one_for_one, 300, 100}, [Server]}}.
+    Server = {p2p_server_server, {p2p_server_server, start_link, [LSocket]},
+              temporary, brutal_kill, worker, [p2p_server_server]},
+
+    RestartStrategy = {simple_one_for_one, 1000, 3600},
+  
+    {ok, {RestartStrategy, [Server]}}.
 
 
 %% ===================================================================
